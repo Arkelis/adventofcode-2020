17f49f146120fbb36f1f024a4c26cbffb6322786
Arkelis <guillaume.fayard@pycolore.fr>
2020-12-03 19:22:07 +0100

    Day 3

---
+++ src/day3.py
@@ -0,0 +1,22 @@
+with open("inputs/day3.txt", "r") as f:
+    lines = list(f)
+
+# Part 1
+
+def count_trees(slope_right, slope_bottom, lines):
+    x = 0
+    trees = 0
+    pattern_width = len(lines[0]) - 1
+    for y in range(0, len(lines), slope_bottom):
+        trees += lines[y][x%pattern_width] == "#"
+        x += slope_right
+    return trees
+
+print("Part 1", count_trees(3, 1, lines))
+print("Part 2", (
+    count_trees(3, 1, lines)
+    * count_trees(1, 1, lines)
+    * count_trees(7, 1, lines)
+    * count_trees(5, 1, lines)
+    * count_trees(1, 2, lines)
+))
