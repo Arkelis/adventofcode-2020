d9c4be479e6d69700e454b799f0950551b0d4050
Guillaume Fayard <guillaume.fayard@pycolore.fr>
2020-12-06 10:47:28 +0100

    Day 6

---
+++ src/day6.py
@@ -0,0 +1,11 @@
+def count_yes(possible_answers, group_answers, need_all=False):
+	if need_all:
+		group_answers = set.intersection(*map(set, group_answers.split(" ")))
+	return sum(q in group_answers for q in possible_answers)
+
+
+if __name__ == "__main__":
+    with open("inputs/day6.txt", "r") as f:
+        lines = (f.read() + "\n").replace("\n", " ").split("  ") # one line per group
+    print("Part 1:", sum(count_yes("abcdefghijklmnopqrstuvwxyz", line) for line in lines))
+    print("Part 2:", sum(count_yes("abcdefghijklmnopqrstuvwxyz", line, True) for line in lines))
