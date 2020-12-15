fc3387cb471ac8f98bef138ba00f05f83932032b
Guillaume Fayard <guillaume.fayard@pycolore.fr>
2020-12-15 09:11:03 +0100

    Day 15 improved

--- python/day15.py
+++ python/day15.py
@@ -1,18 +1,17 @@
 from collections import deque
 
 
-# submited with this, but takes a lot of memory for part 2
-# todo: optimize for large number of turns
 def find_number_of_turn(turn, init_numbers):
-    mem = {n: deque([i], maxlen=2) for i, n in enumerate(init_numbers, 1)}
+    mem = {n: i for i, n in enumerate(init_numbers[:-1], 1)}
     last_number = init_numbers[-1]
-    for i in range(len(init_numbers)+1, turn+1):
-        last_number = (
-            mem[last_number][0] - mem[last_number][1]
-            if len(mem[last_number]) == 2
+    for i in range(len(init_numbers), turn):
+        current_number = (
+            i - mem[last_number]
+            if mem.get(last_number) is not None
             else 0
         )
-        mem.setdefault(last_number, deque([], maxlen=2)).appendleft(i)
+        mem[last_number] = i
+        last_number = current_number
     return last_number
 
 
